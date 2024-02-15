617c4a410adada26d0eef3d130e91c7c59cc55d7
Iskhak401 <moldokasymoviskhak@gmail.com>
2024-02-15 21:36:52 +0600

    1

--- modules/ue-1/main.tf
+++ modules/ue-1/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "first" {
-  vpc_id     = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   cidr_block = "10.0.1.0/24"
 
@@ -27,14 +27,14 @@ resource "aws_subnet" "second" {
 }
 
 resource "aws_internet_gateway" "igw" {
-vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
   tags = {
     Name = "hey"
   }
 }
 
 resource "aws_route_table" "example" {
-vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "route-table-${var.route_table_name}"
@@ -48,15 +48,15 @@ resource "aws_route_table_association" "iii" {
 }
 
 resource "aws_route" "r" {
-  route_table_id            = aws_route_table.example.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.example.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_instance" "qwer" {
-  subnet_id     = aws_subnet.first.id
-  ami           = var.ami
-  instance_type = var.ins_type
+  subnet_id              = aws_subnet.first.id
+  ami                    = var.ami
+  instance_type          = var.ins_type
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
   tags = {
     Name = "EC2"
@@ -64,11 +64,11 @@ resource "aws_instance" "qwer" {
 }
 
 resource "aws_instance" "not" {
-  subnet_id     = aws_subnet.second.id
-  ami           = var.ami2
-  instance_type = var.ins_type2
+  subnet_id              = aws_subnet.second.id
+  ami                    = var.ami2
+  instance_type          = var.ins_type2
   vpc_security_group_ids = [aws_security_group.my_server.id]
-  
+
   tags = {
     Name = "MYec2"
   }
@@ -82,21 +82,21 @@ resource "aws_security_group" "my_webserver" {
   dynamic "ingress" {
     for_each = var.port
     content {
-    from_port        = ingress.value
-    to_port          = ingress.value
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-tags = {
-  Name = "FIrst"
-}
+  tags = {
+    Name = "FIrst"
+  }
 }
 
 
@@ -114,22 +114,22 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_instance" "last-one" {
-  subnet_id     = aws_subnet.second.id
-  ami           = var.ami2
-  instance_type = var.ins_type2
+  subnet_id              = aws_subnet.second.id
+  ami                    = var.ami2
+  instance_type          = var.ins_type2
   vpc_security_group_ids = [aws_security_group.my_server.id]
-  
+
   tags = {
     Name = "MYec2"
   }
 }
 
 resource "aws_instance" "LATEST" {
-  subnet_id     = aws_subnet.second.id
-  ami           = var.ami2
-  instance_type = var.ins_type2
+  subnet_id              = aws_subnet.second.id
+  ami                    = var.ami2
+  instance_type          = var.ins_type2
   vpc_security_group_ids = [aws_security_group.my_server.id]
-  
+
   tags = {
     Name = "MYec2"
   }
