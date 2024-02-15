8b6e2a0630e134d4e07a8629f028d085c9652904
Iskhak401 <moldokasymoviskhak@gmail.com>
2024-02-16 00:40:24 +0600

    1first

---
+++ modules/ue-1/event.tf
@@ -0,0 +1,8 @@
+data "aws_cloudwatch_event_source" "examplepartner" {
+  name_prefix = "aws.partner/examplepartner.com"
+}
+
+resource "aws_cloudwatch_event_bus" "examplepartner" {
+  name              = data.aws_cloudwatch_event_source.examplepartner.name
+  event_source_name = data.aws_cloudwatch_event_source.examplepartner.name
+}
